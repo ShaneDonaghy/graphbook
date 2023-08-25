f7dde66fdcbb72b8cebca249aa42f07f7a60178b
Shane Donaghy <donaghys@tcd.ie>
2023-08-25 16:14:33 +0100

    added send chat message functionality

--- src/client/Chat.js
+++ src/client/Chat.js
@@ -21,11 +21,57 @@ query chat($chatId: Int!) {
 }
 `;
 
+const ADD_MESSAGE = gql`
+    mutation addMessage($message: MessageInput!) {
+        addMessage(message: $message) {
+            id
+            text
+            user {
+                id
+            }
+        }
+    }
+`;
+
 const Chat = (props) => {
     const { chatId, closeChat } = props;
     const { loading, error, data } = useQuery(GET_CHAT, {
         variables: { chatId }
     });
+    const [text, setText] = useState('');
+    const [addMessage] = useMutation(ADD_MESSAGE, {
+        update(cache, { data: { addMessage } }) {
+            cache.modify({
+                id: cache.identify(data.chat),
+                fields: {
+                    messages(existingMessages = []) {
+                        const newMessageRef = cache.writeFragment({
+                            data: addMessage,
+                            fragment: gql`
+                            fragment NewMessage on Chat {
+                                id
+                                type
+                            }
+                            `
+                        });
+                        return [...existingMessages, newMessageRef];
+                    }
+                }
+            });
+        }
+    });
+
+    const handleKeyPress = (event) => {
+        if (event.key === 'Enter' && text.length) {
+            addMessage({ variables: { message: { text, chatId } } }).then(() => {
+                setText('');
+            });
+        }
+    }
+
+
+
+
     if (loading) return <div className="chatWindow">
         <p>Loading . . .</p>
     </div>;
@@ -46,6 +92,10 @@ const Chat = (props) => {
                         {message.text}
                     </div>
                 )}
+            </div>
+            <div className='input'>
+                <input type="text" value={text} onChange={(e) =>
+                    setText(e.target.value)} onKeyPress={handleKeyPress} />
             </div>
         </div>
     )
--- src/client/Feed.js
+++ src/client/Feed.js
@@ -1,15 +1,19 @@
 import React, { useState } from 'react';
 import { gql, useQuery, useMutation } from '@apollo/client';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
-const GET_POSTS = gql`{
-    posts {
-        id
-        text
-        user {
-            avatar
-            username
+const GET_POSTS = gql`
+    query postsFeed($page: Int, $limit: Int) {
+        postsFeed(page: $page, limit: $limit) {
+            posts {
+                id
+                text
+                user {
+                    avatar
+                    username
+                }
+            }
         }
-    }
 }`;
 
 const ADD_POST = gql`
@@ -42,26 +46,65 @@ const Feed = () => {
         update(cache, { data: { addPost } }) {
             cache.modify({
                 fields: {
-                    posts(existingPosts = []) {
+                    postsFeed(existingPostsFeed) {
+                        const { posts: existingPosts } = existingPostsFeed;
                         const newPostRef = cache.writeFragment({
                             data: addPost,
                             fragment: gql`
-                        fragment NewPost on Post {
-                            id
-                            type
-                        }`
+                                fragment NewPost on Post {
+                                    id
+                                    type
+                                }
+                            `
                         });
-                        return [newPostRef, ...existingPosts];
+                        return {
+                            ...existingPostsFeed,
+                            posts: [newPostRef, ...existingPosts]
+                        };
                     }
                 }
             });
         }
     });
-    const { loading, error, data } = useQuery(GET_POSTS);
+    const [hasMore, setHasMore] = useState(true);
+    const [page, setPage] = useState(0);
+    const loadMore = (fetchMore) => {
+        const self = this;
+        fetchMore({
+            variables: {
+                page: page + 1,
+            },
+            updateQuery(previousResult, { fetchMoreResult }) {
+                if (!fetchMoreResult.postsFeed.posts.length) {
+                    setHasMore(false);
+                    return previousResult;
+                }
+                setPage(page + 1);
+                const newData = {
+                    postsFeed: {
+                        __typename: 'PostFeed',
+                        posts: [
+                            ...previousResult.postsFeed.posts,
+                            ...fetchMoreResult.postsFeed.posts
+                        ]
+                    }
+                };
+                return newData;
+            }
+        });
+    }
+    const { loading, error, data, fetchMore } = useQuery(GET_POSTS, {
+        pollInterval: 5000,
+        variables: {
+            page: 0,
+            limit: 10
+        }
+    });
     const [postContent, setPostContent] = useState('');
     if (loading) return 'Loading...';
     if (error) return `Error: ${error.message}`;
-    const { posts } = data;
+    const { postsFeed } = data;
+    const { posts } = postsFeed;
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -78,15 +121,22 @@ const Feed = () => {
                 </form>
             </div>
             <div className="feed">
-                {posts.map((post, i) =>
-                    <div key={post.id} className={'post ' + (post.id < 0 ? 'optimistic' : '')}>
-                        <div className="header">
-                            <img src={post.user.avatar} />
-                            <h2>{post.user.username}</h2>
+                <InfiniteScroll
+                    dataLength={posts.length}
+                    next={() => loadMore(fetchMore)}
+                    hasMore={hasMore}
+                    loader={<div className="loader" key={"loader"}>Loading . . .</div>}
+                >
+                    {posts.map((post, i) =>
+                        <div key={post.id} className={'post ' + (post.id < 0 ? 'optimistic' : '')}>
+                            <div className="header">
+                                <img src={post.user.avatar} />
+                                <h2>{post.user.username}</h2>
+                            </div>
+                            <p className="content">{post.text}</p>
                         </div>
-                        <p className="content">{post.text}</p>
-                    </div>
-                )}
+                    )}
+                </InfiniteScroll>
             </div>
         </div>
     )
